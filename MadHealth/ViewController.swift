f1962ac04db974e521b9b7f5948be1525dc6a504
Andrei Tselihuzau <wapsthefast@gmail.com>
2022-11-18 11:49:12 +0300

    Initial Commit

---
+++ MadHealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MadHealth
+//
+//  Created by Андрэй Целігузаў on 18.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
